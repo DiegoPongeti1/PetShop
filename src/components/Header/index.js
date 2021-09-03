d405154b4cce7747a9be1c66976a3d5119a44c02
Diego Pongeti <diego-p43@hotmail.com>
2021-09-03 13:23:15 -0300

    Iniciando Front com styled components

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+import { AreaHeader } from './styled'
+
+function Header() { 
+    return (
+        <AreaHeader>
+            Topo so site 
+        </AreaHeader>  
+    )
+}
+
+export default Header;
